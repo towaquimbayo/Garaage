40a063e415982f75a5c83436e4cf8800fd748148
noufilsaqib <noufil.s@hotmail.com>
2024-07-14 05:16:16 -0700

    Added vector images

---
+++ lib/core/config/assets/app_vectors.dart
@@ -0,0 +1,7 @@
+class AppVectors {
+  static const String basePath = 'assets/vectors';
+  static const String format = '.svg';
+
+  static const String topPattern = '$basePath/top_pattern$format';
+  static const String bottomPattern = '$basePath/bottom_pattern$format';
+}
