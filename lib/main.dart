a445175af87e3992e70054cbb9e8c7e895e0fa6d
Mangat Singh Toor | ਮੰਗਤ ਸਿੰਘ ਤੂਰ <immangat@gmail.com>
2024-07-15 12:13:56 -0700

    add chatbot and gemini set up

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gemini/flutter_gemini.dart';
+import 'package:garaage/conts.dart';
+import 'package:garaage/presentation/chatbot/pages/chatbot.dart';
 
 import 'core/config/theme/app_theme.dart';
 
 void main() {
+  Gemini.init(
+    apiKey: GEMINI_API_KEY,
+  );
   runApp(const MyApp());
 }
 
@@ -12,9 +18,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: AppTheme.lightTheme,
-      debugShowCheckedModeBanner: false,
-      home: Container()
-    );
+        theme: AppTheme.lightTheme,
+        debugShowCheckedModeBanner: false,
+        home: const Chatbot());
   }
 }
