d4aa7de2bf357bae855d2fd0941e25f62a82dd80
Mangat Singh Toor | ਮੰਗਤ ਸਿੰਘ ਤੂਰ <immangat@gmail.com>
2024-07-15 23:47:25 -0700

    add microphone input support to chatbot

--- lib/presentation/chatbot/pages/chatbot.dart
+++ lib/presentation/chatbot/pages/chatbot.dart
@@ -4,8 +4,9 @@ import 'dart:typed_data';
 import 'package:dash_chat_2/dash_chat_2.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gemini/flutter_gemini.dart';
-import 'package:garaage/core/config/theme/app_colors.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:speech_to_text/speech_recognition_result.dart';
+import 'package:speech_to_text/speech_to_text.dart';
 
 class Chatbot extends StatefulWidget {
   const Chatbot({super.key});
@@ -15,10 +16,41 @@ class Chatbot extends StatefulWidget {
 }
 
 class _ChatbotState extends State<Chatbot> {
+  final TextEditingController _controller = TextEditingController();
+  final SpeechToText _speechToText = SpeechToText();
+  bool _speechEnabled = false;
   final Gemini gemini = Gemini.instance;
   List<ChatMessage> messages = [];
   ChatUser currentUser = ChatUser(id: "0", firstName: "User");
   ChatUser geminiUser = ChatUser(id: "1", firstName: "Gemini");
+
+  @override
+  void initState() {
+    super.initState();
+    initSpeech();
+  }
+
+  void initSpeech() async {
+    _speechEnabled = await _speechToText.initialize();
+    setState(() {});
+  }
+
+  void _startListening() async {
+    await _speechToText.listen(onResult: _onSpeechResult);
+    setState(() {});
+  }
+
+  void _stopListening() async {
+    await _speechToText.stop();
+    setState(() {});
+  }
+
+  void _onSpeechResult(SpeechRecognitionResult? result) {
+    final speechInput = result?.recognizedWords ?? "";
+    setState(() {
+      _controller.text = speechInput;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -44,12 +76,13 @@ class _ChatbotState extends State<Chatbot> {
         )
       ], trailing: [
         IconButton(
-          onPressed: () {},
-          icon: const Icon(
-            Icons.mic,
+          onPressed:
+              _speechToText.isListening ? _stopListening : _startListening,
+          icon: Icon(
+            _speechToText.isListening ? Icons.mic_off : Icons.mic,
           ),
         )
-      ]),
+      ], textController: _controller),
       currentUser: currentUser,
       onSend: _sendMessage,
       messages: messages,
@@ -121,7 +154,7 @@ class _ChatbotState extends State<Chatbot> {
       ChatMessage message = ChatMessage(
           user: currentUser,
           createdAt: DateTime.now(),
-          text: "Describe this Image",
+          text: "Return the code in the image",
           medias: [
             ChatMedia(url: file.path, fileName: "", type: MediaType.image)
           ]);
