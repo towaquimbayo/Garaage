34147ad92b3a91ddb7e6a985c268a7cbcaae6f86
noufilsaqib <noufil.s@hotmail.com>
2024-07-20 16:04:24 -0700

    Updated theme with input fields styling

--- lib/common/widgets/my_button.dart
+++ lib/common/widgets/my_button.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:garaage/core/config/theme/app_colors.dart';
 
 class MyButton extends StatelessWidget {
   final VoidCallback onPressed;
   final String type;
   final String text;
-  final double ? height;
+  final double? height;
+  final String? leftIcon;
+  final String? rightIcon;
 
   const MyButton({
     super.key,
@@ -13,6 +16,8 @@ class MyButton extends StatelessWidget {
     required this.type,
     required this.text,
     this.height,
+    this.leftIcon,
+    this.rightIcon,
   });
 
   @override
@@ -21,10 +26,34 @@ class MyButton extends StatelessWidget {
       onPressed: onPressed,
       style: ElevatedButton.styleFrom(
         minimumSize: Size.fromHeight(height ?? 48),
-        backgroundColor: type == "primary" ? AppColors.primary : type == "secondary" ? AppColors.surface : AppColors.background,
-        foregroundColor: type == "primary" ? AppColors.surface : AppColors.headingText,
+        backgroundColor: _getBackgroundColor(),
+        foregroundColor: _getForegroundColor(),
       ),
-      child: Text(text),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          if (leftIcon != null) SvgPicture.asset(leftIcon!),
+          const Spacer(),
+          Text(text),
+          const Spacer(),
+          if (rightIcon != null) SvgPicture.asset(rightIcon!),
+        ],
+      ),
     );
+  }
+
+  Color _getBackgroundColor() {
+    switch (type) {
+      case "primary":
+        return AppColors.primary;
+      case "secondary":
+        return AppColors.surface;
+      default:
+        return AppColors.background;
+    }
+  }
+
+  Color _getForegroundColor() {
+    return type == "primary" ? AppColors.surface : AppColors.headingText;
   }
 }
--- lib/presentation/auth/pages/register.dart
+++ lib/presentation/auth/pages/register.dart
@@ -1,14 +1,280 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import '../../../common/widgets/my_app_bar.dart';
+import '../../../common/widgets/my_button.dart';
+import '../../../core/config/assets/app_icons.dart';
+import '../../../core/config/assets/app_vectors.dart';
+import '../../../core/config/theme/app_colors.dart';
+import '../../../core/config/theme/app_text.dart';
+import 'sign_in.dart';
 
 class RegisterPage extends StatelessWidget {
   const RegisterPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Register Page'),
+    return Scaffold(
+      appBar: MyAppBar(
+        title: SvgPicture.asset(
+          AppVectors.logo,
+          colorFilter: const ColorFilter.mode(
+            AppColors.primary,
+            BlendMode.srcIn,
+          ),
+          height: 24,
+        )
       ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(height: 24),
+            _registerText(),
+            const SizedBox(height: 24),
+            _nameFields(context),
+            const SizedBox(height: 16),
+            _emailField(context),
+            const SizedBox(height: 16),
+            _passwordFields(context),
+            const SizedBox(height: 24),
+            _acknowledgeTerms(),
+            const SizedBox(height: 24),
+            MyButton(
+              type: 'primary',
+              text: 'Register',
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (BuildContext context) => const SignInPage(),
+                  ),
+                );
+              },
+            ),
+            const SizedBox(height: 4),
+            _redirectText(context),
+            const SizedBox(height: 8),
+            _withGoogle(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _registerText() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Text(
+          'Register',
+          style: AppText.headH2,
+        ),
+        Text(
+          'Create an account to get started',
+          style: AppText.bodyS.copyWith(color: AppColors.bodyText),
+        ),
+      ],
+    );
+  }
+
+  Widget _nameFields(BuildContext context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(bottom: 8),
+                child: Text(
+                  'First Name',
+                  style: AppText.headH5.copyWith(color: AppColors.headingText),
+                ),
+              ),
+              TextField(
+                decoration: const InputDecoration(
+                  hintText: 'John',
+                ).applyDefaults(
+                  Theme.of(context).inputDecorationTheme,
+                ),
+              ),
+            ],
+          ),
+        ),
+        const SizedBox(width: 16),
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(bottom: 8),
+                child: Text(
+                  'Last Name',
+                  style: AppText.headH5.copyWith(color: AppColors.headingText),
+                ),
+              ),
+              TextField(
+                decoration: const InputDecoration(
+                  hintText: 'Doe',
+                ).applyDefaults(
+                  Theme.of(context).inputDecorationTheme,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _emailField(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(bottom: 8),
+          child: Text(
+            'Email',
+            style: AppText.headH5.copyWith(color: AppColors.headingText),
+          ),
+        ),
+        TextField(
+          decoration: const InputDecoration(
+            hintText: 'name@email.com',
+          ).applyDefaults(
+            Theme.of(context).inputDecorationTheme,
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _passwordFields(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(bottom: 8),
+          child: Text(
+            'Password',
+            style: AppText.headH5.copyWith(color: AppColors.headingText),
+          ),
+        ),
+        TextField(
+          decoration: const InputDecoration(
+            hintText: 'Create a password',
+          ).applyDefaults(
+            Theme.of(context).inputDecorationTheme,
+          ),
+        ),
+        const SizedBox(height: 16),
+        TextField(
+          decoration: const InputDecoration(
+            hintText: 'Confirm password',
+          ).applyDefaults(
+            Theme.of(context).inputDecorationTheme,
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _acknowledgeTerms() {
+    return Row(
+      children: [
+        Transform.scale(
+          scale: 1.5,
+          child: Checkbox(
+            value: false,
+            side: const BorderSide(color: AppColors.lightGrayDark),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(6),
+            ),
+            onChanged: (value) {
+              
+            },
+            materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+            visualDensity: VisualDensity.compact,
+          ),
+        ),
+        const SizedBox(width: 8),
+        Flexible(
+          child: Text.rich(
+            TextSpan(
+              children: [
+                TextSpan(
+                  text: 'I\'ve read and agree with the ',
+                  style: AppText.bodyS.copyWith(color: AppColors.darkGrayLight),
+                ),
+                TextSpan(
+                  text: 'Terms and Conditions',
+                  style: AppText.actionM.copyWith(color: AppColors.primary),
+                ),
+                TextSpan(
+                  text: ' and the ',
+                  style: AppText.bodyS.copyWith(color: AppColors.darkGrayLight),
+                ),
+                TextSpan(
+                  text: 'Privacy Policy',
+                  style: AppText.actionM.copyWith(color: AppColors.primary),
+                ),
+                TextSpan(
+                  text: '.',
+                  style: AppText.bodyS.copyWith(color: AppColors.darkGrayLight),
+                ),
+              ],
+            ),
+            softWrap: true,
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _redirectText(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          'Already have an account?',
+          style: AppText.bodyS.copyWith(color: AppColors.bodyText),
+        ),
+        TextButton(
+          onPressed: () {
+            Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (BuildContext context) => const SignInPage(),
+            ),
+          );
+          },
+          child: Text(
+            'Sign In',
+            style: AppText.actionM.copyWith(color: AppColors.primary),
+          ),
+        ),
+      ],
+    );
+  }
+  
+  Widget _withGoogle() {
+    return Column(
+      children: [
+        Container(
+          height: 1,
+          color: AppColors.lightGrayMedium,
+        ),
+        const SizedBox(height: 24),
+        MyButton(
+          type: 'secondary',
+          text: 'Continue with Google',
+          leftIcon: AppIcons.broken['google'],
+          onPressed: () {},
+        )
+      ],
     );
   }
 }
