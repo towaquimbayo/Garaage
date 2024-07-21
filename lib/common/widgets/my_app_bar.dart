8ae4a5855047fdf6aa8c6757819a25c68766c98c
Towa Quimbayo <towacurtis@hotmail.com>
2024-07-20 17:07:32 -0700

    added state boolean vars for navbar

--- lib/common/widgets/my_app_bar.dart
+++ lib/common/widgets/my_app_bar.dart
@@ -3,12 +3,17 @@ import 'package:flutter_svg/flutter_svg.dart';
 
 import '../../core/config/theme/app_colors.dart';
 import '../../core/config/assets/app_icons.dart';
+import '../../presentation/profile/pages/profile.dart';
 
 class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
   final Widget? title;
+  final bool? leading;
+  final bool? actions;
   const MyAppBar({
     super.key,
     this.title,
+    this.leading,
+    this.actions,
   });
 
   @override
@@ -21,29 +26,59 @@ class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
         elevation: 0,
         title: title ?? const Text(''),
         centerTitle: true,
-        leading: IconButton(
-          icon: Container(
-            decoration: const BoxDecoration(
-              color: AppColors.surface,
-              shape: BoxShape.circle,
-            ),
-            padding: const EdgeInsets.all(8),
-            child: SvgPicture.asset(
-              AppIcons.broken['arrow-left']!,
-              colorFilter: const ColorFilter.mode(
-                AppColors.headingText,
-                BlendMode.srcIn,
-              ),
-            ),
-          ),
-          onPressed: () {
-            Navigator.pop(context);
-          },
-        ),
+        leading: leading == true
+            ? IconButton(
+                icon: Container(
+                  decoration: const BoxDecoration(
+                    color: AppColors.surface,
+                    shape: BoxShape.circle,
+                  ),
+                  padding: const EdgeInsets.all(8),
+                  child: SvgPicture.asset(
+                    AppIcons.broken['arrow-left']!,
+                    colorFilter: const ColorFilter.mode(
+                      AppColors.headingText,
+                      BlendMode.srcIn,
+                    ),
+                  ),
+                ),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              )
+            : const SizedBox(),
+        actions: actions == true
+            ? [
+                IconButton(
+                  icon: Container(
+                    decoration: const BoxDecoration(
+                      color: AppColors.surface,
+                      shape: BoxShape.circle,
+                    ),
+                    padding: const EdgeInsets.all(8),
+                    child: SvgPicture.asset(
+                      AppIcons.broken['user']!,
+                      colorFilter: const ColorFilter.mode(
+                        AppColors.headingText,
+                        BlendMode.srcIn,
+                      ),
+                    ),
+                  ),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (BuildContext context) => const ProfilePage(),
+                      ),
+                    );
+                  },
+                ),
+              ]
+            : const [],
       ),
     );
   }
-  
+
   @override
   Size get preferredSize => const Size.fromHeight(kToolbarHeight);
 }
