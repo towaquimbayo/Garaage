ddc18bc5e2d3a5a5cf976a65f888563f9926c9d6
noufilsaqib <noufil.s@hotmail.com>
2024-07-22 23:29:35 -0700

    Scaffolded profile page

--- lib/presentation/profile/pages/profile.dart
+++ lib/presentation/profile/pages/profile.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+import '../../../common/widgets/my_app_bar.dart';
+import '../../../core/config/theme/app_colors.dart';
+import '../../../core/config/theme/app_text.dart';
+
 class ProfilePage extends StatelessWidget {
   static String routeName = '/profile';
   
@@ -7,6 +11,18 @@ class ProfilePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: MyAppBar(
+        leading: true,
+        backgroundColor: AppColors.surface,
+        title: Text(
+          'Profile',
+          style: AppText.pageTitleText.copyWith(color: AppColors.headingText),
+        ),
+      ),
+      body: const Center(
+        child: Text('Welcome to the Profile Page'),
+      ),
+    );
   }
 }
