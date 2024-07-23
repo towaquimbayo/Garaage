17918d434159efee8bb38f027421590b1627c0e5
noufilsaqib <noufil.s@hotmail.com>
2024-07-23 12:43:12 -0700

    Implemented confirm when exiting app functionality

--- lib/presentation/navigation/pages/navigation.dart
+++ lib/presentation/navigation/pages/navigation.dart
@@ -1,7 +1,11 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:garaage/core/config/theme/app_colors.dart';
 
 import '../../../common/widgets/my_bottom_nav_bar.dart';
+import '../../../core/config/theme/app_text.dart';
 import '../../diagnostics/pages/diagnostics.dart';
 import '../../home/pages/home.dart';
 import '../bloc/navigation_cubit.dart';
@@ -13,13 +17,51 @@ class NavigationPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<NavigationCubit, NavBarItem>(
-      builder: (BuildContext context, state) {
-        return Scaffold(
-          body: _buildBody(state),
-          bottomNavigationBar: const MyBottomNavBar(),
-        );
+    return PopScope(
+      canPop: false,
+      onPopInvoked: (didPop) async {
+        if (didPop) {
+          return;
+        }
+        final result = await _showExitConfirmationDialog(context);
+        if (result == true) {
+          if (context.mounted) Navigator.of(context).pop();
+        }
       },
+      child: BlocBuilder<NavigationCubit, NavBarItem>(
+        builder: (BuildContext context, state) {
+          return Scaffold(
+            body: _buildBody(state),
+            bottomNavigationBar: const MyBottomNavBar(),
+          );
+        },
+      ),
+    );
+  }
+
+  Future<bool?> _showExitConfirmationDialog(BuildContext context) {
+    return showDialog<bool>(
+      context: context,
+      builder: (context) =>  AlertDialog(
+        title: const Text(
+          'Exit App',
+          style: AppText.headH1,
+        ),
+        content: const Text('Are you sure you want to exit the app?'),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.of(context).pop(false),
+            child: const Text('No', style: TextStyle(color: AppColors.placeholderText)),
+          ),
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop(true);
+              exit(0);
+            },
+            child: const Text('Yes'),
+          ),
+        ],
+      ),
     );
   }
 
