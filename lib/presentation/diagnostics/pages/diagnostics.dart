bc87f76057b3f003da9632895c636337f234492d
Towa Quimbayo <towacurtis@hotmail.com>
2024-07-24 01:37:20 -0700

    Created mock error code json list

--- lib/presentation/diagnostics/pages/diagnostics.dart
+++ lib/presentation/diagnostics/pages/diagnostics.dart
@@ -7,6 +7,42 @@ import '../../../core/config/theme/app_text.dart';
 class DiagnosticsPage extends StatelessWidget {
   const DiagnosticsPage({super.key});
 
+  // @TODO: Replace sample Error codes with DB / Bloc data
+  static final errorCodes = [
+    {
+      'code': 'P0128',
+      'description':
+          'The engine coolant isn\'t reaching optimal temperature due to a stuck-open thermostat, causing rich fuel mixture, increased fuel consumption, and reduced performance.',
+      'causes': [
+        'Stuck-open thermostat',
+        'Faulty coolant temperature sensor (CTS)',
+        'Low coolant level',
+        'Air in the cooling system',
+      ],
+      'solutions': [
+        'Check the coolant level: Top up if needed.',
+        'Bleed the cooling system. To remove trapped air.',
+        'Test the thermostat: Replace if it fails to open and close properly.',
+        'Inspect the coolant temperature sensor (CTS): Replace if faulty.',
+      ],
+      'partsNeeded': [
+        {
+          'name': 'Replacement thermostat',
+          'price': 6.99,
+          'vendor': 'AutoZone',
+          'link':
+              'https://www.autozone.com/cooling-heating-and-climate-control/thermostat/duralast-thermostat-15368/10007_0_0',
+        },
+        {
+          'name': 'Coolant',
+          'price': 14.99,
+          'vendor': 'NAPA',
+          'link': 'https://www.napaonline.com/en/p/ZERCLC1',
+        }
+      ],
+    },
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
