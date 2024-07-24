9824281a1221376de82e5fa02a722242459520f8
Mangat Singh Toor | ਮੰਗਤ ਸਿੰਘ ਤੂਰ <immangat@gmail.com>
2024-07-24 11:21:57 -0700

    modify build body of navbar

--- lib/presentation/navigation/pages/navigation.dart
+++ lib/presentation/navigation/pages/navigation.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:garaage/core/config/theme/app_colors.dart';
+import 'package:garaage/presentation/chatbot/pages/chatbot.dart';
 
 import '../../../common/widgets/my_bottom_nav_bar.dart';
 import '../../../core/config/theme/app_text.dart';
@@ -12,7 +13,7 @@ import '../bloc/navigation_cubit.dart';
 
 class NavigationPage extends StatelessWidget {
   static String routeName = '/navigation';
-  
+
   const NavigationPage({super.key});
 
   @override
@@ -42,7 +43,7 @@ class NavigationPage extends StatelessWidget {
   Future<bool?> _showExitConfirmationDialog(BuildContext context) {
     return showDialog<bool>(
       context: context,
-      builder: (context) =>  AlertDialog(
+      builder: (context) => AlertDialog(
         title: const Text(
           'Exit App',
           style: AppText.headH1,
@@ -51,7 +52,8 @@ class NavigationPage extends StatelessWidget {
         actions: [
           TextButton(
             onPressed: () => Navigator.of(context).pop(false),
-            child: const Text('No', style: TextStyle(color: AppColors.placeholderText)),
+            child: const Text('No',
+                style: TextStyle(color: AppColors.placeholderText)),
           ),
           TextButton(
             onPressed: () {
@@ -71,6 +73,8 @@ class NavigationPage extends StatelessWidget {
         return const HomePage();
       case NavBarItem.diagnostics:
         return const DiagnosticsPage();
+      case NavBarItem.chatbot:
+        return const ChatbotPage();
       default:
         return const HomePage();
     }
