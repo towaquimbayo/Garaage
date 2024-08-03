ba63ff67361b078acfa20d4439de18d407739d89
Mangat Singh Toor | ਮੰਗਤ ਸਿੰਘ ਤੂਰ <immangat@gmail.com>
2024-08-03 00:04:47 -0700

    add selected images widget for showing multiple selected images.

--- lib/presentation/chatbot/widgets/selected_images.dart
+++ lib/presentation/chatbot/widgets/selected_images.dart
@@ -1,3 +1,4 @@
+import 'dart:io';
 import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
@@ -8,62 +9,94 @@ import '../../../core/config/assets/app_icons.dart';
 import '../../../core/config/theme/app_colors.dart';
 
 class SelectedImages extends StatelessWidget {
-  const SelectedImages(
-      {super.key,
-      required this.size,
-      required this.selectedImages,
-      required this.removeImage});
+  SelectedImages({
+    Key? key,
+    required List<XFile?> pickedImages,
+    required this.size,
+    required this.removeImage,
+  })  : _pickedImages = pickedImages,
+        super(key: key);
 
   final double size;
-  final List<XFile?> selectedImages;
+  final List<XFile?> _pickedImages;
   final Function(int) removeImage;
+
+  Future<List<Uint8List>> _loadImages() async {
+    return await Future.wait(
+      _pickedImages.where((file) => file != null).map((file) async {
+        final bytes = await File(file!.path).readAsBytes();
+        return bytes;
+      }),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: size * 0.4,
-      child: ListView.builder(
-        padding: EdgeInsets.symmetric(
-          vertical: 5,
-          horizontal: 5,
-        ),
-        itemBuilder: (BuildContext context, int index) {
-          return FutureBuilder<Uint8List>(
-            future: selectedImages[index]?.readAsBytes(),
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return CircularProgressIndicator();
-              } else if (snapshot.hasError) {
-                return Text('Error loading image');
-              } else {
+    return FutureBuilder<List<Uint8List>>(
+      future: _loadImages(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(child: CircularProgressIndicator());
+        } else if (snapshot.hasError) {
+          return Center(child: Text('Error loading images'));
+        } else {
+          final selectedImages = snapshot.data!;
+          return SizedBox(
+            height: size * 0.4,
+            child: ListView.builder(
+              padding: const EdgeInsets.symmetric(
+                vertical: 5,
+                horizontal: 5,
+              ),
+              itemBuilder: (BuildContext context, int index) {
                 return Padding(
                   padding: const EdgeInsets.all(1.0),
-                  child: Stack(
-                    alignment: AlignmentDirectional.topEnd,
-                    children: [
-                      Image.memory(snapshot.data!),
-                      IconButton(
-                        onPressed: () {
-                          removeImage(index);
-                        },
-                        icon: SvgPicture.asset(
-                          height: size * 0.1,
-                          AppIcons.broken['forbidden']!,
-                          colorFilter: const ColorFilter.mode(
-                            AppColors.surface,
-                            BlendMode.srcIn,
+                  child: Dismissible(
+                    direction: DismissDirection.down,
+                    background: Container(
+                      color: Colors.red,
+                    ),
+                    key: UniqueKey(),
+                    onDismissed: (direction) {
+                      removeImage(index);
+                    },
+                    child: Stack(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(4.0),
+                          child: Image.memory(
+                            selectedImages[index],
                           ),
                         ),
-                      ),
-                    ],
+                        Positioned(
+                          right: 0,
+                          top: 0,
+                          height: 15,
+                          width: 15,
+                          child: GestureDetector(
+                            onTap: () {
+                              removeImage(index);
+                            },
+                            child: SvgPicture.asset(
+                              AppIcons.bold['close']!,
+                              colorFilter: const ColorFilter.mode(
+                                AppColors.warningDark,
+                                BlendMode.srcIn,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 );
-              }
-            },
+              },
+              itemCount: selectedImages.length,
+              scrollDirection: Axis.horizontal,
+            ),
           );
-        },
-        itemCount: selectedImages.length,
-        scrollDirection: Axis.horizontal,
-      ),
+        }
+      },
     );
   }
 }
