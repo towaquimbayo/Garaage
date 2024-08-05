7eb0f0c6630afceef2ed6314f3ccc38979fd7868
Mangat Singh Toor | ਮੰਗਤ ਸਿੰਘ ਤੂਰ <immangat@gmail.com>
2024-08-04 23:32:13 -0700

    update to add diagnostic chat function and history

--- lib/presentation/chatbot/bloc/chatbot_cubit.dart
+++ lib/presentation/chatbot/bloc/chatbot_cubit.dart
@@ -4,7 +4,6 @@ import 'dart:typed_data';
 import 'package:dartz/dartz.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:dash_chat_2/dash_chat_2.dart';
-import 'package:image_picker/image_picker.dart';
 
 import '../../../data/models/chat/ai_message_request.dart';
 import '../../../data/models/chat/ai_message_response.dart';
@@ -16,9 +15,6 @@ part 'chatbot_state.dart';
 class ChatbotCubit extends Cubit<ChatbotState> {
   ChatbotCubit() : super(ChatbotState(chatMessages: []));
   final gemini = sl<GetAiMessage>();
-
-  ChatUser currentUser = ChatUser(id: "0", firstName: "User");
-  ChatUser geminiUser = ChatUser(id: "1", firstName: "Mika");
 
   void addChatMessage(
     ChatMessage message,
@@ -39,10 +35,15 @@ class ChatbotCubit extends Cubit<ChatbotState> {
             .map((file) => File(file.url).readAsBytesSync())
             .toList();
       }
+
       gemini
           .call(
-              params: AiMessageRequest(
-                  requestMessageText: question, images: images))
+        params: AiMessageRequest(
+          requestMessageText: question,
+          images: images,
+          chatHistory: state.chatMessages,
+        ),
+      )
           .then((event) {
         event.fold(
           (l) => _onSuccess(l, state.chatMessages),
@@ -54,9 +55,51 @@ class ChatbotCubit extends Cubit<ChatbotState> {
     }
   }
 
+  void startDiagnosticChat(Map<String, Object> diagnosticIssue) {
+    print(diagnosticIssue);
+    final initialChatMessages =
+        getInitialDiagnosticMessages(diagnosticIssue['code'] as String);
+    emit(
+      ChatbotState(
+        chatMessages: initialChatMessages,
+      ),
+    );
+    try {
+      gemini
+          .call(
+        params: AiMessageRequest(
+          requestMessageText: _formatPrompt(
+            diagnosticIssue,
+          ),
+          chatHistory: state.chatMessages,
+        ),
+      )
+          .then((event) {
+        event.fold(
+          (l) => _onSuccess(l, state.chatMessages),
+          (r) => _onFail(r),
+        );
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  String _formatPrompt(Map<String, Object> diagnosticIssue) {
+    return """
+    Provide a set of instructions related to my diagnostic issue of a car.
+    Instructions should keep in mind that the user might be a beginner and not
+    completely used to cars and/or have extensive knowledge of them.
+    Also assure the user you can always provide more help or guidance.
+    
+    Below is the Json for the diagnostic issue:
+    ${diagnosticIssue.toString()}
+    """;
+  }
+
   void _onSuccess(AiMessageResponse l, List<ChatMessage> messages) {
     ChatMessage? lastMessage = messages.firstOrNull;
-    if (lastMessage != null && lastMessage.user == geminiUser) {
+    if (lastMessage != null && lastMessage.user == ChatbotState.geminiUser) {
       lastMessage = messages.removeAt(0);
       // String response = event.content?.parts?.fold(
       //     "", (previous, current) => "$previous ${current.text}") ??
@@ -75,7 +118,7 @@ class ChatbotCubit extends Cubit<ChatbotState> {
 
       final response = l.aiMessageResponseText;
       ChatMessage message = ChatMessage(
-        user: geminiUser,
+        user: ChatbotState.geminiUser,
         createdAt: DateTime.now(),
         text: response,
         isMarkdown: true,
