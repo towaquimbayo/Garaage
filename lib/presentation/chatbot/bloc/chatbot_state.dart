75da0e173c22ef2624ec9bd3ca9b45fa41a3ffc9
Mangat Singh Toor | ਮੰਗਤ ਸਿੰਘ ਤੂਰ <immangat@gmail.com>
2024-08-04 23:32:44 -0700

    add static users and initial diagnostic message array

--- lib/presentation/chatbot/bloc/chatbot_state.dart
+++ lib/presentation/chatbot/bloc/chatbot_state.dart
@@ -2,6 +2,17 @@ part of 'chatbot_cubit.dart';
 
 class ChatbotState {
   final List<ChatMessage> chatMessages;
+  static ChatUser currentUser = ChatUser(id: "0", firstName: "User");
+  static ChatUser geminiUser = ChatUser(id: "1", firstName: "Mika");
 
   ChatbotState({required this.chatMessages});
 }
+
+getInitialDiagnosticMessages(String diagnosticName) => [
+      ChatMessage(
+        text: "Hey, could you help me with the issue **$diagnosticName**.",
+        isMarkdown: true,
+        user: ChatbotState.currentUser,
+        createdAt: DateTime.now(),
+      )
+    ];
