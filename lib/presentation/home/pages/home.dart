dfb79c3763ab72aa690ed1641cd28eeca927b2a8
Towa Quimbayo <towacurtis@hotmail.com>
2024-08-07 18:43:54 -0700

    Created func to fetch Vehicle from Firestore

--- lib/presentation/home/pages/home.dart
+++ lib/presentation/home/pages/home.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 import '../../../core/config/assets/app_images.dart';
 import '../../../common/widgets/my_app_bar.dart';
@@ -21,26 +23,39 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  Map<String, dynamic> vehicle = {
-    'status': 'Disconnected', // Connected | Disconnected
-    'name': 'Honda Civic',
-    'description': '2021 Sport Hybrid Edition',
-    'image': Image.asset(
-      AppImages.hondaCivic,
-      fit: BoxFit.contain,
-    ),
-    'transmission': 'Auto',
-    'numSeats': 5,
-    'errors': 0,
-    'fuelConsumed': 70,
-    'totalFuel': 100,
-    'speed': 68,
-    'rpm': 2731,
-    'battery': 84,
-    'oil': 59,
-    'coolantCurrent': 90,
-    'coolantDesired': 120,
-  };
+  Map<String, dynamic> vehicle = {};
+
+  @override
+  void initState() {
+    super.initState();
+    final user = FirebaseAuth.instance.currentUser;
+    if (user?.uid != null) fetchVehicleData(user!.uid);
+  }
+
+  // Fetch vehicle data from Firestore using the user ID
+  Future<void> fetchVehicleData(String userId) async {
+    try {
+      DocumentSnapshot vehicleData = await FirebaseFirestore.instance
+          .collection('Users')
+          .doc(userId)
+          .collection('Vehicles')
+          .doc('1NXBR32E85Z505904') // @TOOD: Replace hardcoded VIN
+          .get();
+
+      if (vehicleData.exists) {
+        setState(() {
+          vehicle = vehicleData.data() as Map<String, dynamic>;
+          // @TODO: Remove this when image is stored in Firestore
+          vehicle['image'] = Image.asset(
+            AppImages.hondaCivic,
+            fit: BoxFit.contain,
+          );
+        });
+      }
+    } catch (e) {
+      ErrorHandler.handleError(context, e.toString());
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -75,87 +90,89 @@ class _HomePageState extends State<HomePage> {
       ),
       body: SingleChildScrollView(
         padding: const EdgeInsets.symmetric(horizontal: 15),
-        child: Column(
-          children: [
-            VehicleCard(
-              name: vehicle['name'] as String,
-              description: vehicle['description'] as String,
-              image: vehicle['image'] as Widget,
-              errors: vehicle['errors'] as int,
-              transmission: vehicle['transmission'] as String,
-              numSeats: vehicle['numSeats'] as int,
-              status: vehicle['status'] as String,
-            ),
-            const SizedBox(height: 10),
-            FuelConsumptionCard(
-              currentConsumed: vehicle['status'] == 'Disconnected'
-                  ? 0
-                  : vehicle['fuelConsumed'] as int,
-              totalConsumed: vehicle['totalFuel'] as int,
-            ),
-            const SizedBox(height: 10),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Expanded(
-                  flex: 4,
-                  child: VehicleStatsCard(
-                    value: vehicle['speed'] as int,
-                    icon: AppIcons.broken['speed']!,
-                    mainLabel: 'Speed',
-                    subLabel: 'km/h',
+        child: vehicle.isEmpty
+            ? const Center(child: Text('Hmm... No vehicle data found'))
+            : Column(
+                children: [
+                  VehicleCard(
+                    name: vehicle['name'] as String,
+                    description: vehicle['description'] as String,
+                    image: vehicle['image'] as Widget,
+                    errors: vehicle['errors'] as int,
+                    transmission: vehicle['transmission'] as String,
+                    numSeats: vehicle['numSeats'] as int,
+                    status: vehicle['status'] as String,
                   ),
-                ),
-                const SizedBox(width: 10),
-                Expanded(
-                  flex: 6,
-                  child: VehicleStatsCard(
-                    value: vehicle['rpm'] as int,
-                    icon: AppIcons.broken['rpm']!,
-                    mainLabel: 'Engine RPM',
-                    fixAlignment: true,
+                  const SizedBox(height: 10),
+                  FuelConsumptionCard(
+                    currentConsumed: vehicle['status'] == 'Disconnected'
+                        ? 0
+                        : vehicle['fuelConsumed'] as int,
+                    totalConsumed: vehicle['totalFuel'] as int,
                   ),
-                ),
-              ],
-            ),
-            const SizedBox(height: 10),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Expanded(
-                  flex: 6,
-                  child: VehicleStatsCard(
-                    value: vehicle['battery'] as int,
-                    icon: AppIcons.broken['battery']!,
-                    mainLabel: 'Car Battery',
-                    postfix: '%',
+                  const SizedBox(height: 10),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Expanded(
+                        flex: 4,
+                        child: VehicleStatsCard(
+                          value: vehicle['speed'] as int,
+                          icon: AppIcons.broken['speed']!,
+                          mainLabel: 'Speed',
+                          subLabel: 'km/h',
+                        ),
+                      ),
+                      const SizedBox(width: 10),
+                      Expanded(
+                        flex: 6,
+                        child: VehicleStatsCard(
+                          value: vehicle['rpm'] as int,
+                          icon: AppIcons.broken['rpm']!,
+                          mainLabel: 'Engine RPM',
+                          fixAlignment: true,
+                        ),
+                      ),
+                    ],
                   ),
-                ),
-                const SizedBox(width: 10),
-                Expanded(
-                  flex: 4,
-                  child: VehicleStatsCard(
-                    value: vehicle['oil'] as int,
-                    icon: AppIcons.broken['drop']!,
-                    mainLabel: 'Oil',
-                    postfix: '%',
+                  const SizedBox(height: 10),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Expanded(
+                        flex: 6,
+                        child: VehicleStatsCard(
+                          value: vehicle['battery'] as int,
+                          icon: AppIcons.broken['battery']!,
+                          mainLabel: 'Car Battery',
+                          postfix: '%',
+                        ),
+                      ),
+                      const SizedBox(width: 10),
+                      Expanded(
+                        flex: 4,
+                        child: VehicleStatsCard(
+                          value: vehicle['oil'] as int,
+                          icon: AppIcons.broken['drop']!,
+                          mainLabel: 'Oil',
+                          postfix: '%',
+                        ),
+                      ),
+                    ],
                   ),
-                ),
-              ],
-            ),
-            const SizedBox(height: 10),
-            VehicleStatsCard(
-              value: vehicle['coolantCurrent'] as int,
-              valueAlt: vehicle['coolantDesired'] as int,
-              icon: AppIcons.broken['coolant']!,
-              mainLabel: 'Coolant Temp',
-              subLabel: 'current',
-              subLabelAlt: 'desired',
-              postfix: '°C',
-            ),
-            const SizedBox(height: 20),
-          ],
-        ),
+                  const SizedBox(height: 10),
+                  VehicleStatsCard(
+                    value: vehicle['coolantCurrent'] as int,
+                    valueAlt: vehicle['coolantDesired'] as int,
+                    icon: AppIcons.broken['coolant']!,
+                    mainLabel: 'Coolant Temp',
+                    subLabel: 'current',
+                    subLabelAlt: 'desired',
+                    postfix: '°C',
+                  ),
+                  const SizedBox(height: 20),
+                ],
+              ),
       ),
     );
   }
